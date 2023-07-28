1b7de1d07a382ad3f264932228f1bd1bb3292df7
alifirhas <irhasalif@gmail.com>
2023-07-28 23:10:33 +0700

    :children_crossing: Implement tailwind and daisyUI styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{html,js}", 
-		"/index.html"
-	],
+	content: ["./src/**/*.{html,js}", "./index.html"],
 	theme: {
 		extend: {},
+	},
+	daisyui: {
+		themes: ["light", "dark", "cupcake", "forest"],
 	},
 	plugins: [require("daisyui")],
 };
